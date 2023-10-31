37f602fac3e06b7adabe3f808f71445c65db0a91
Alghifari203 <149274172+Alghifari203@users.noreply.github.com>
2023-10-31 21:59:06 +0700

    Update Housing_app.py

--- Housing_app.py
+++ Housing_app.py
@@ -7,11 +7,17 @@ st.title('Stock Price Prediction App')
 total_rooms = st.number_input('Input total rooms')
 total_bedrooms = st.number_input('Input total bedrooms')
 longitude = st.number_input('Input longitude ')
-
+latitude = st.number_input('Input latitude')
+population = st.number_input('Input population')
+housing_median_age = st.number_input('Input housing median age')
+households = st.number_input('Input households')
+median_income = st.number_input('Input median income')
+median_house_value = st.number_input('Input median house value')
+ocean_proximity =  st.number_input('Input ocean proximity')
 predict = ''
 
 if st.button('predict'):
     predict = model.predict(
-        [[total_rooms, total_bedrooms, longitude]]
+        [[total_rooms, total_bedrooms, longitude, latitude, population, housing_median_age, households, median_income, median_house_value, ocean_proximity ]]
     )
     st.write('population : ', predict)
